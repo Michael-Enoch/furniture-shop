3733bbdbec165c160bb7fbc16e64b37dd15d44bc
MichaeL Enoch <mike.saviour08@gmail.com>
2025-06-11 08:33:01 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-
-import { gsap } from "gsap";
-
 import { Toaster } from 'react-hot-toast';
 import './App.css';
 import { AuthProvider } from './context/AuthContext';
@@ -11,7 +8,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {CartPage } from './pages/CartPage';
 import SearchPage from './pages/SearchPage';
-import Breadcrumbs from './components/BreadCrumbs';
+
 
 
 function App() {
