6b284c14c166956e9306f778741b2ae8be6e6012
MichaeL Enoch <mike.saviour08@gmail.com>
2025-06-20 14:09:02 +0100

    Updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -576,7 +576,7 @@ const Navbar = () => {
                               setMobileMenuOpen(false);
                             }}
                           >
-                            {label}
+                            {label.name}
                           </Link>
                         ))}
                       </motion.div>
