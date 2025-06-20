24b757c62245abbbde75fa65ce029c4dc4cced71
Melody <melodycollins579@gmail.com>
2025-06-20 05:57:54 -0700

    WIP: Updating App and Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
-feature/contact-page
-import { Toaster } from 'react-hot-toast'
-import './App.css'
-import { AuthProvider } from './context/AuthContext'
-import Register from './pages/RegisterForm'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import Homepage from './pages/Homepage'
-import ContactPage from './pages/ContactPage'
-
-
-main
-
-import { gsap } from "gsap";
-
+// import { gsap } from "gsap";
 import { Toaster } from 'react-hot-toast';
 import './App.css';
 import { AuthProvider } from './context/AuthContext';
@@ -21,21 +7,13 @@ import Login from './pages/Login';
 import Homepage from './pages/Homepage';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {CartPage } from './pages/CartPage';
+import { CartPage } from './pages/CartPage';
 import SearchPage from './pages/SearchPage';
-import Breadcrumbs from './components/BreadCrumbs';
-
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
     <AuthProvider>
-feature/contact-page
-    <Toaster position='top-right' reverseOrder={false}/>
-    <Navbar/>
-    <Homepage/>
-    <ContactPage/>
-      {/* <Register/>
-      <Login/> */}
       <Router>
         <Toaster position="top-right" reverseOrder={false} />
         <Navbar />
@@ -45,9 +23,9 @@ feature/contact-page
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
-main
     </AuthProvider>
   );
 }
