82afd3d4d8ce4bb4c910f19857854c4f7e070d60
MichaeL Enoch <mike.saviour08@gmail.com>
2025-06-27 08:07:48 +0100

    updated

--- src/routes/RoleProtectedRoute.jsx
+++ src/routes/RoleProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
+
 const RoleProtectedRoute = ({ allowedRoles }) => {
   const { currentUser, userData, loading } = useAuth();
 
