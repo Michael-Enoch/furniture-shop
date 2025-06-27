80f8968082eddfce775376bca6f131772d7bf6c8
MichaeL Enoch <mike.saviour08@gmail.com>
2025-06-27 16:13:12 +0100

    updated review link routing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -158,7 +158,7 @@ const Navbar = () => {
   };
   const moreLinks = [
     { name: "Site map", to: "/site-map" },
-    { name: "Reviews", to: "/" },
+    { name: "Reviews", to: "/reviews" },
     { name: "Help Center", to: "/" },
     { name: "FAQ", to: "/faq" },
   ];
