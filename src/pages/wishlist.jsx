eda65a86d92b0516e35f8811f1c88b87726cba0c
MichaeL Enoch <mike.saviour08@gmail.com>
2025-06-27 08:12:42 +0100

    updated styling

--- src/pages/wishlist.jsx
+++ src/pages/wishlist.jsx
@@ -1,42 +1,94 @@
 import { useWishlist } from "../context/WishlistContext";
 import { ShoppingCart, Trash2 } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import theme from "../context/Theme";
+import { useEffect } from "react";
+import Breadcrumbs from "../components/BreadCrumbs";
+import { useAuth } from "../context/AuthContext";
+import { useCart } from "../context/CartContext";
+import { toast } from "sonner";
 
 export const Wishlist = () => {
   const { wishlist, toggleWishlist } = useWishlist();
+  const {currentUser} = useAuth();
+  const {addToCart} = useCart();
+  const navigate = useNavigate();
 
-  const addToCart = (product) => {
-    const cart = JSON.parse(localStorage.getItem("cart")) || [];
-    const existing = cart.find((item) => item.id === product.id);
-    if (existing) {
-      existing.quantity += 1;
-    } else {
-      cart.push({ ...product, quantity: 1 });
+    useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
+
+    useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [toggleWishlist]);
+
+  const handleAddToCart = (product) => {
+    if (!currentUser) {
+      toast.error("⚠️ You need to sign in to add items to your cart.", {
+        position: "top-right",
+        style: {
+          backgroundColor: "#3A2F2A",
+          color: "#F8F5F2",
+          border: "1px solid #A65A2E",
+          padding: "14px",
+          fontSize: "13px",
+          borderRadius: "8px",
+        },
+        duration: 2000,
+      });
+      setTimeout(() => {
+        navigate("/login");
+      }, 1500);
+      return;
     }
-    localStorage.setItem("cart", JSON.stringify(cart));
-    alert(`${product.name} added to cart`);
+
+    addToCart(product);
+
+    toast.success(`✅ Added to cart!`, {
+      position: "bottom-center",
+      style: {
+        backgroundColor: "#3A2F2A",
+        color: "#F8F5F2",
+        border: "1px solid #A65A2E",
+        padding: "14px",
+        fontSize: "13px",
+        borderRadius: "8px",
+      },
+      iconTheme: {
+        primary: "#A65A2E",
+        secondary: "#F8F5F2",
+      },
+      duration: 3000,
+      description: "View your cart to checkout.",
+      action: {
+        label: "View Cart",
+        onClick: () => navigate("/cart"),
+      },
+    });
   };
 
   if (!wishlist.length) {
     return (
-      <div className="min-h-[500px] flex flex-col justify-center items-center text-center">
-        <h2 className="text-2xl font-bold">Your wishlist is empty 💔</h2>
-        <Link
-          to="/products"
-          className="mt-4 inline-block px-6 py-3 bg-[#A65A2E] text-white rounded-2xl hover:bg-opacity-80"
-        >
-          Browse Products
-        </Link>
+      <div className="min-h-[500px] py-10 px-4 w-full max-w-screen-2xl flex flex-col items-center justify-center text-center text-[#2D2D2D]">
+        <Breadcrumbs />
+        <div className="w-full flex flex-col items-center justify-center gap-2">
+          <h2 className="text-2xl font-bold">Your wishlist is empty 💔</h2>
+          <Link
+            to="/products"
+            className="text-[#A65A2E] hover:underline text-lg"
+          >
+            Browse Products
+          </Link>
+        </div>
       </div>
     );
   }
 
   return (
-    <div className="max-w-screen-2xl mx-auto px-4 py-8">
+    <section  className="w-full max-w-screen-2xl px-8 py-14 mx-auto">
       <h1
         className="text-3xl font-semibold mb-6"
-        style={{ color: theme.colors.text }}
+        style={{ color: theme.colors.text.primary }}
       >
         My Wishlist
       </h1>
@@ -74,7 +126,7 @@ export const Wishlist = () => {
 
             <div className="flex gap-2 mt-auto">
               <button
-                onClick={() => addToCart(product)}
+                onClick={() => handleAddToCart(product)}
                 className="flex-1 bg-[#3A2F2A] text-white rounded-xl px-4 py-2 hover:bg-opacity-80 transition text-sm flex items-center justify-center gap-1"
               >
                 <ShoppingCart size={16} /> Add to Cart
@@ -89,6 +141,6 @@ export const Wishlist = () => {
           </div>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
